bbd31268eb58fb244f4dbdc2e4ea72b27dc6d018
struva <75508003+struva@users.noreply.github.com>
2021-08-14 23:35:12 +0300

    update 14_08_21
    0.55.0_x64

--- opera.sh
+++ opera.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 apt install unzip
-wget https://github.com/iteufel/nwjs-ffmpeg-prebuilt/releases/download/0.45.1/0.45.1-linux-x64.zip
-unzip 0.45.1-linux-x64.zip
+wget https://github.com/iteufel/nwjs-ffmpeg-prebuilt/releases/download/0.55.0/0.55.0-linux-x64.zip
+unzip 0.55.0-linux-x64.zip
 mv libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/
-rm -f 0.45.1-linux-x64.zip*
+rm -f 0.55.0-linux-x64.zip*
 
 
     
