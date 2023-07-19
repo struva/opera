4b2b04187f8f550a424220a205d887a1751b1a61
struva <75508003+struva@users.noreply.github.com>
2023-07-19 13:31:22 +0300

    Update opera.sh

--- opera.sh
+++ opera.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 # apt install unzip
-wget https://github.com/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases/download/0.72.0/0.72.0-linux-x64.zip
-unzip 0.72.0-linux-x64.zip
+wget https://github.com/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases/download/0.77.0/0.77.0-linux-x64.zip
+unzip 0.77.0-linux-x64.zip
 # mv libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/ (debian, ubuntu) (/usr/lib/opera - manjaro)
 mv libffmpeg.so /usr/lib/opera
-rm -f 0.72.0-linux-x64.zip*
+rm -f 0.77.0-linux-x64.zip*
 
 
     
